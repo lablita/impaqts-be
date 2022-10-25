9845f6dbaf87dbfc9d33d39bad16f6eec999465e
Marco Fabbri <marco.fabbri@drwolf.it>
2022-10-25 08:42:42 +0200

    :truck: renamed ws endpoint

--- app/it/drwolf/impaqtsbe/controllers/QueryController.java
+++ app/it/drwolf/impaqtsbe/controllers/QueryController.java
@@ -1,9 +1,5 @@
 package it.drwolf.impaqtsbe.controllers;
 
-import java.io.IOException;
-
-import javax.inject.Inject;
-
 import akka.actor.ActorSystem;
 import akka.stream.Materializer;
 import it.drwolf.impaqtsbe.actors.ExternalProcessActor;
@@ -16,6 +12,9 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
 import play.mvc.WebSocket;
+
+import javax.inject.Inject;
+import java.io.IOException;
 
 public class QueryController extends Controller {
 
@@ -43,18 +42,19 @@ public class QueryController extends Controller {
 		try {
 			return Results.ok(this.wrapperCaller.retrieveMetadatumValues(queryRequest));
 		} catch (IOException e) {
-			return Results.internalServerError(String
-					.format("Error retrieving metadatum values for metadatum %s and corpus %s", metadatum, corpus));
+			return Results.internalServerError(
+					String.format("Error retrieving metadatum values for metadatum %s and corpus %s", metadatum,
+							corpus));
 		}
 	}
 
-	public WebSocket testSingleWordExt() throws InterruptedException {
+	public WebSocket getWebSocket() {
 		return WebSocket.Json.accept(request -> ActorFlow.actorRef(
 				out -> ExternalProcessActor.props(out, this.startup.getManateeRegistryPath(),
 						this.startup.getManateeLibPath(), this.startup.getJavaExecutable(),
 						this.startup.getWrapperPath(), this.startup.getDockerSwitch(),
-						this.startup.getDockerManateeRegistry(), this.startup.getDockerManateePath()),
-				this.actorSystem, this.materializer));
+						this.startup.getDockerManateeRegistry(), this.startup.getDockerManateePath()), this.actorSystem,
+				this.materializer));
 	}
 
 }
